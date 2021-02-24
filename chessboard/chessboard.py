fdba16b9c67e662356ddf45036f44d86e894ae3f
BenKMorley <bkm1n18@soton.ac.uk>
2021-02-24 19:27:31 +0000

    Cleaned things up a lot, removed the secondary binary piece arrays in favor of a method

--- chessboard/chessboard.py
+++ chessboard/chessboard.py
@@ -1,6 +1,7 @@
 import numpy
 from chessboard.pieces import piece_moves
 
+
 class Chessboard(object):
   def __init__(self):
     # Define an array containing the names of all of the pieces
@@ -11,72 +12,28 @@ class Chessboard(object):
       for j in range(8):
         self.possible_moves[i, j] = {}
 
-    # Add a piece dictionary
-    self.white_pieces = {}
-    self.black_pieces = {}
-
-    # 0 indexes white and 1 indexes black
     # Note I use the convention whereby the array printed is in the same layout
     # as the chessboard
-    self.white_pieces['white pawn'] = numpy.zeros((8, 8))
-    self.white_pieces['white pawn'][6, :] = numpy.ones(8)
     self.piece_names[6, :] = "white pawn"
-
-    self.black_pieces['black pawn'] = numpy.zeros((8, 8))
-    self.black_pieces['black pawn'][1, :] = numpy.ones(8)
     self.piece_names[1, :] = "black pawn"
-
-    self.white_pieces['white rook'] = numpy.zeros((8, 8))
-    self.white_pieces['white rook'][7, 0] = 1
-    self.white_pieces['white rook'][7, 7] = 1
     self.piece_names[7, 0] = "white rook"
     self.piece_names[7, 7] = "white rook"
-
-    self.black_pieces['black rook'] = numpy.zeros((8, 8))
-    self.black_pieces['black rook'][0, 0] = 1
-    self.black_pieces['black rook'][0, 7] = 1
     self.piece_names[0, 0] = "black rook"
     self.piece_names[0, 7] = "black rook"
-
-    self.white_pieces['white bishop'] = numpy.zeros((8, 8))
-    self.white_pieces['white bishop'][7, 2] = 1
-    self.white_pieces['white bishop'][7, 5] = 1
     self.piece_names[7, 2] = "white bishop"
     self.piece_names[7, 5] = "white bishop"
-
-    self.black_pieces['black bishop'] = numpy.zeros((8, 8))
-    self.black_pieces['black bishop'][0, 2] = 1
-    self.black_pieces['black bishop'][0, 5] = 1
     self.piece_names[0, 2] = "black bishop"
     self.piece_names[0, 5] = "black bishop"
-
-    self.white_pieces['white knight'] = numpy.zeros((8, 8))
-    self.white_pieces['white knight'][7, 1] = 1
-    self.white_pieces['white knight'][7, 6] = 1
     self.piece_names[7, 1] = "white knight"
     self.piece_names[7, 6] = "white knight"
-
-    self.black_pieces['black knight'] = numpy.zeros((8, 8))
-    self.black_pieces['black knight'][0, 1] = 1
-    self.black_pieces['black knight'][0, 6] = 1
     self.piece_names[0, 1] = "black knight"
     self.piece_names[0, 6] = "black knight"
-   
-    self.white_pieces['white king'] = numpy.zeros((8, 8))
-    self.white_pieces['white king'][7, 4] = 1
     self.piece_names[7, 4] = "white king"
-
-    self.black_pieces['black king'] = numpy.zeros((8, 8))
-    self.black_pieces['black king'][0, 4] = 1
     self.piece_names[0, 4] = "black king"
-
-    self.white_pieces['white queen'] = numpy.zeros((8, 8))
-    self.white_pieces['white queen'][7, 3] = 1
     self.piece_names[7, 3] = "white queen"
-
-    self.black_pieces['black queen'] = numpy.zeros((8, 8))
-    self.black_pieces['black queen'][0, 3] = 1
     self.piece_names[0, 3] = "black queen"
+
+    self.find_all_moves()
 
   def find_all_moves(self):
     for i in range(8):
@@ -87,33 +44,70 @@ class Chessboard(object):
   def move(self, start, finish):
     start = tuple(start)
     finish = tuple(finish)
-    # pdb.set_trace()
-
-    # Check if a piece has been captured
-    color = self.piece_names[start][0:5]
-
-    if self.piece_names[finish] != "":
-      if color == "white":
-        self.black_pieces[self.piece_names[finish]] = 0
-         
-      if color == "black":
-        self.white_pieces[self.piece_names[finish]] = 0
 
     # Update piece position
     name = self.piece_names[start]
     self.piece_names[start] = ''
     self.piece_names[finish] = name
-
-    color = name[0:5]
-    if color == 'white':
-      self.white_pieces[name][start] = 0
-      self.white_pieces[name][finish] = 1
-    
-    else:
-      self.black_pieces[name][start] = 0
-      self.black_pieces[name][finish] = 1
     
     self.find_all_moves()
 
-  def get_positions(self):
-    return self.piece_names
+  def construct_piece_binary_arrays(self):
+    """
+      I'm going to use the first index of the binary array to represent the
+      type of piece. The key for this is as follows:
+
+      0: white pawn
+      1: white knight
+      2: white bishop
+      3: white rook
+      4: white queen
+      5: white king
+      6: black pawn
+      7: black knight
+      8: black bishop
+      9: black rook
+      10: black queen
+      11: black king
+    """
+    piece_array_binary = numpy.zeros((12, 8, 8))
+
+    for i in range(8):
+      for j in range(8):
+        if self.piece_names[i, j] == "white pawn":
+          piece_array_binary[0, i, j] = 1
+
+        if self.piece_names[i, j] == "white knight":
+          piece_array_binary[1, i, j] = 1
+
+        if self.piece_names[i, j] == "white bishop":
+          piece_array_binary[2, i, j] = 1
+
+        if self.piece_names[i, j] == "white rook":
+          piece_array_binary[3, i, j] = 1
+
+        if self.piece_names[i, j] == "white queen":
+          piece_array_binary[4, i, j] = 1
+
+        if self.piece_names[i, j] == "white king":
+          piece_array_binary[5, i, j] = 1
+
+        if self.piece_names[i, j] == "black pawn":
+          piece_array_binary[6, i, j] = 1
+
+        if self.piece_names[i, j] == "black knight":
+          piece_array_binary[7, i, j] = 1
+
+        if self.piece_names[i, j] == "black bishop":
+          piece_array_binary[8, i, j] = 1
+
+        if self.piece_names[i, j] == "black rook":
+          piece_array_binary[9, i, j] = 1
+
+        if self.piece_names[i, j] == "black queen":
+          piece_array_binary[10, i, j] = 1
+
+        if self.piece_names[i, j] == "black king":
+          piece_array_binary[11, i, j] = 1
+
+    return piece_array_binary
